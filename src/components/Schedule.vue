aeefd3ef31871343a415f61197ec2eb78ef57c08
ajr <aromano@fsu.edu>
2019-09-30 15:34:42 -0400

    updated schedule link

---
+++ src/components/Schedule.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <p>Upcoming Events at the dhlab</p>
+        <p>
+        <iframe src="https://dhlab.skedda.com/booking?embedded=true" style="width: 100%; height: 1000px;"></iframe>
+        </p>
+    </div>
+</template>
+
